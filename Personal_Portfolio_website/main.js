2ad5611f2d328534fbb8156552ce74bb081b33a8
Dayana Armas <dyarmasb@hotmail.com>
2022-07-23 12:29:35 -0500

    upload

---
+++ Personal_Portfolio_website/main.js
@@ -0,0 +1,52 @@
+/*menu show and hidden */
+const  navMenu = document.getElementById('nav-menu'),
+       navToggle = document.getElementById('nav-toggle'),
+       navClose = document.getElementById('nav-close');
+
+/*MENU SHOW*/
+/*VALIDATE IF CONSTANT EXISTS*/
+if(navToggle){
+    console.log("testing");
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*MENU HIDDEN*/
+/*VALIDATE IF CONSTANT EXISTS*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*REMOVE MENU MOBILE*/
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*ACORDION SKILLS*/
+
+const   skillsContent = document.getElementsByClassName('skills_content'),
+        skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if (itemClass === 'skills_content skills_close' ) {
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
